618679298b64e63caacc47b93583cc21bdf38c4c
William Breindel <williambreindel@gmail.com>
2020-04-14 11:57:44 -0400

    First Commit

---
+++ src/build/precache-manifest.f8c3da0c14cbb38cfe581cce6900565e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30898a131c452a27b4b0133f7947e75e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3411a2347180628f8e6b",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "7f78f3fa23121d3cfe08",
+    "url": "/static/js/2.b58cde9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b58cde9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3411a2347180628f8e6b",
+    "url": "/static/js/main.d666e981.chunk.js"
+  },
+  {
+    "revision": "616d08cd1d5b39c91497",
+    "url": "/static/js/runtime-main.246ecd58.js"
+  }
+]);
